491aba49de273a1ff12bc422cbc367d542964ee2
Nick AMancini <nmancini11@gmail.com>
2014-02-09 01:26:48 -0800

    updating galleries pages

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
+    $('.gallery').hide();
 
     function initialize() {
-      var myLatlng = new google.maps.LatLng(37.4206, -121.8917);
-      var mapOptions = {
-        zoom: 13,
-        center: new google.maps.LatLng(37.4315, -121.8917),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById('maps-canvas'),
-          mapOptions);
+        var myLatlng = new google.maps.LatLng(37.4206, -121.8917);
+        var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(37.4315, -121.8917),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById('maps-canvas'),
+        mapOptions);
 
-      var contentString = '<div id="content">'+
-      '<h1>B.T. Mancini Headquarters</h1>'+
-      '<div id="bodyContent">'+
-      '<span>876 Milpitas Blvd.</span><br>'+'<span>Milpitas, CA</span>'+
-      '</div>'+
-      '</div>';
+        var contentString = '<div id="content">' +
+            '<h1>B.T. Mancini Headquarters</h1>' +
+            '<div id="bodyContent">' +
+            '<span>876 Milpitas Blvd.</span><br>' + '<span>Milpitas, CA</span>' +
+            '</div>' +
+            '</div>';
 
         var infowindow = new google.maps.InfoWindow({
             content: contentString
@@ -26,12 +27,12 @@ $(document).ready(function () {
             map: map,
             title: 'B.T. Mancini Inc'
         });
-        infowindow.open(map,marker);
+        infowindow.open(map, marker);
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
 
-        function closeSafety() {
+    function closeSafety() {
         $('#safety .container, #bar a').fadeOut(500);
         $('#safety, #safety .container').addClass('safety-squish');
         $('#safety .container').removeClass('fadeInDown');
@@ -52,67 +53,81 @@ $(document).ready(function () {
     }
 
     function gallerySlider() {
-    $(".sidebar li:not(.slider)").click(function () {
-        $(".slider").animate({
-                top: $(this).offset().top,
-                height: $(this).height()
-            });
-        });
-    }
 
-    $('.project').on('click', gallerySlider);
+      
+        var classy = $(this).data("id"),
+        searchGallery = '.gallery[data-id=' + classy + ']',
+        newGallery = $(searchGallery);
+        
+        if($(this).hasClass('selected')){
+            $(this).removeClass('selected').find('.dropdown').slideToggle();
+        }else{
+            $('.selected').removeClass('selected').closest('.project').find('.dropdown').slideToggle();
+            $(this).addClass('selected').find('.dropdown').slideToggle();
+        }
 
-    $('#safe, option').click(function () {
-        openSafety();
-    });
+        var pastGallery = $('.gallery[data-active=true]')
+        if(pastGallery.length > 0 && pastGallery != newGallery){
+            pastGallery.slideUp().removeClass('fadeInDown').addClass('animated fadeOutUp').removeAttr('data-active');
+        }
+        if(pastGallery != newGallery){
+            newGallery.slideDown().removeClass('fadeOutUp').addClass('animated fadeInDown').attr('data-active', true);
+        }
+}
 
-    $('#btm-logo').mouseenter(function(){
-        $('#btm-logo').addClass('animated rotateIn');
-    });
+$('.project').on('click', gallerySlider);
 
-    $('#btm-logo').mouseleave(function(){
-        $('#btm-logo').removeClass('rotateIn');
-    });
+$('#safe, option').click(function () {
+    openSafety();
+});
 
-    $('.csi-code').click(function () {
-        $('.products, #products-bar').slideUp('slow');
-        $('#division-subsection').slideDown('slow').css('height', '100%');
-        $('.csi-codes, #csi-bar').slideDown('slow').css('display', 'block');
-    });
+$('#btm-logo').mouseenter(function () {
+    $('#btm-logo').addClass('animated rotateIn');
+});
 
-    $('#csi-bar').click(function() {
-        $('.csi-codes, #csi-bar').slideUp('slow');
-    })
+$('#btm-logo').mouseleave(function () {
+    $('#btm-logo').removeClass('rotateIn');
+});
 
-    $('.products-services').click(function () {
-        //showText();
-        $('.csi-codes, #csi-bar').slideUp('slow');
-        $('#division-subsection').slideDown('slow').css('height', '100%');
-        $('.products, #products-bar').slideDown('slow').css('display', 'block');
-    });
+$('.csi-code').click(function () {
+    $('.products, #products-bar').slideUp('slow');
+    $('#division-subsection').slideDown('slow').css('height', '100%');
+    $('.csi-codes, #csi-bar').slideDown('slow').css('display', 'block');
+});
 
-    $('#products-bar').click(function() {
-        $('.products, #products-bar').slideUp('slow');
-    })
+$('#csi-bar').click(function () {
+    $('.csi-codes, #csi-bar').slideUp('slow');
+});
 
-    $('.division').mouseenter(function () {
-        //showText();
-        $('.division-text, a.division-link', this).removeClass('fadeOutDown');
-        $('.division-text, a.division-link', this).css('display', 'block');
-        $('.division').not(this).fadeTo( "slow" , 0.8);
-        $('.division-text, a.division-link', this).addClass('animated fadeInUp');
+$('.products-services').click(function () {
+    //showText();
+    $('.csi-codes, #csi-bar').slideUp('slow');
+    $('#division-subsection').slideDown('slow').css('height', '100%');
+    $('.products, #products-bar').slideDown('slow').css('display', 'block');
+});
 
-    });
-    $('.division').mouseleave(function () {
-        $('.division-text, a.division-link', this).removeClass('fadeInUp');
-        $('.division').fadeTo( "fast" , 1);
-        $('.division-text, a.division-link', this).addClass('animated fadeOutDown');
-    });
+$('#products-bar').click(function () {
+    $('.products, #products-bar').slideUp('slow');
+});
 
-    $('#bar').click(function () {
-        closeSafety();
-    });
+$('.division').mouseenter(function () {
+    //showText();
+    $('.division-text, a.division-link', this).removeClass('fadeOutDown');
+    $('.division-text, a.division-link', this).css('display', 'block');
+    $('.division').not(this).fadeTo("slow", 0.8);
+    $('.division-text, a.division-link', this).addClass('animated fadeInUp');
 
-    
+});
+$('.division').mouseleave(function () {
+    $('.division-text, a.division-link', this).removeClass('fadeInUp');
+    $('.division').fadeTo("fast", 1);
+    $('.division-text, a.division-link', this).addClass('animated fadeOutDown');
+});
+
+$('#bar').click(function () {
+    closeSafety();
+});
+
+
 closeSafety();
 });
