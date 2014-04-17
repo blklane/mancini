b2d20e290103fdbbdec5d228c836db9ed6f69fcc
Nick AMancini <nmancini11@gmail.com>
2014-04-16 23:11:55 -0700

    layout complete... a little rought around the edges. CleanSSing & Minification coming soon...

---
+++ app/assets/javascripts/min/application-ck.js
@@ -0,0 +1,1 @@
+$(function(){$(document).foundation({orbit:{animation:"slide",timer:!0,timer_speed:3e3,next_on_click:!0,slide_number:!1,variable_height:!0,animation_speed:800,navigation_arrows:!1,bullets:!1}})});
